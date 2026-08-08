7e701ff2039fe3cb1ad549554ce10c3a9e071635
humbeteli <humbeteliqurbanov@gmail.com>
2026-08-09 02:05:30 +0400

    add logout

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,4 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
 const Tasks = () => {
-    return <h1>Tapşırıqlar</h1>
-}
-export default Tasks
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+
+    navigate("/login");
+  };
+
+  return (
+    <>
+      <h1>Tapşırıqlar</h1>
+
+      <button onClick={handleLogout}>Çıxış et</button>
+    </>
+  );
+};
+
+export default Tasks;
