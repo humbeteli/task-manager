0924fc6a3b66349e477d6d6c2a237640193d8d3b
humbeteli <humbeteliqurbanov@gmail.com>
2026-08-10 20:52:21 +0400

    checkpoint 6 tamamlandı error boundary əlavə olundu

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,26 @@ import Login from "./pages/Login";
 import Tasks from "./pages/Tasks";
 import { Navigate } from "react-router-dom";
 import ProtectedRoute from "./routes/ProtectedRoute";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<Login />} />
+      <ErrorBoundary>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<Login />} />
 
-        <Route
-          path="/tasks"
-          element={
-            <ProtectedRoute>
-              <Tasks />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
+          <Route
+            path="/tasks"
+            element={
+              <ProtectedRoute>
+                <Tasks />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </ErrorBoundary>
     </BrowserRouter>
   );
 };
